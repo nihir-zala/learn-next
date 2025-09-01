8abe456ef9391538be79882bd5b2936e35b6e592
Nihir <nihir.z@evolutionloud.in>
2025-09-01 18:11:31 +0530

    added the project section animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,18 @@
 import { twMerge } from "tailwind-merge";
 import grainImage from "@/assets/images/grain.jpg";
-import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef, PropsWithChildren } from "react";
 export const Card = ({
   className,
   children,
-}: PropsWithChildren<{
-  className?: string;
-  children?: React.ReactNode;
-}>) => {
+  ...other
+}: ComponentPropsWithoutRef<"div">) => {
   return (
     <div
       className={twMerge(
         "bg-gray-800 rounded-3xl z-0 after:z-10 overflow-hidden relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
         className
       )}
+      {...other}
     >
       <div
         className="absolute inset-0 -z-10 opacity-5"
